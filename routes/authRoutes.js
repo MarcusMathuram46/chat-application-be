2446cd358126403c7a9e623e3e4c72bcf90e5e3f
Marcus Mathuram <marcus.mathuram7@gmail.com>
2024-06-26 17:58:50 +0530

    Done

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -5,7 +5,7 @@ const router = require('express').Router();
 router.post("/login", login);
 router.post("/register", register);
 router.get("/allUsers/:id", getAllUsers);
-router.post("/setAvatar/:id", setAvatar);
+router.post("/setavatar/:id", setAvatar);
 router.get("/logout", logout);
 
 module.exports = router;
